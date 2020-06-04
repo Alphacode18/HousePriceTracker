8f2f08b764934f37273a5b3304f3dfa7996946de
Alphacode18 <skharban@purdue.edu>
2020-06-04 19:17:31 +0530

    Auto price tracker logic added

--- script.js
+++ script.js
@@ -21,13 +21,32 @@ async function sendMail() {
     console.log("Message sent: %s", info.messageId);
 }
 
-sendMail();
-
-let url;
-
 async function scrapePrice(url) {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url, {waitUntil: 'networkidle0'});
+    const operation = await page.evaluate(() => {
+      const price = document.getElementsByClassName('price__value  u-ignore-phonenumber');
+      return price[0].innerText;
+    });
+    console.log(operation);
     await browser.close();
+    return operation;
 }
+
+async function priceTracker(url, desiredPrice) {
+  let emailSent = false;
+  let price = scrapePrice(url);
+  if (price >= price) {
+    sendMail();
+    emailSent = true;
+    return emailSent;
+  }
+}
+
+async function main() {
+  let sentValue = priceTracker('https://www.sothebysrealty.com/eng/sales/detail/180-l-1187-hnyp6r/249-south-westgate-avenue-los-angeles-ca-90049', 10000000);
+  setTimeout(() => {console.log("Fetching Data");}, 2000);
+}
+
+main();
